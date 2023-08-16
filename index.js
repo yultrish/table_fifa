de63c3c3964c73b2c385658a89e6605decf2960c
yultrish <yultrish@gmail.com>
2023-08-16 11:23:38 +0000

    update

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ window.addEventListener('load', ()=>{
     setTimeout(()=>{
         teams(
            fifa = [
-        {Ranks:1, Team:'Brazil', totalPoints:1.841, Rank:16, Team:'Uruguay', totalPoints:1.627},
-        {Ranks:2, Team:'Argentina', totalPoints:1.83, Rank:17, Team:'Seraleon', totalPoints:1.5},
-        {Ranks:3, Team:'Morroco', totalPoints:1.821, Rank:18, Team:'Ghana', totalPoints:1.45},
+        {Ranks:1, Teams:'Brazil', totalPoint:1.841, Rank:16, Team:'Uruguay', totalPoints:1.627},
+        {Ranks:2, Teams:'Argentina', totalPoint:1.83, Rank:17, Team:'Seraleon', totalPoints:1.5},
+        {Ranks:3, Teams:'Morroco', totalPoint:1.821, Rank:18, Team:'Ghana', totalPoints:1.45},
             ]
         )
     }, 5000)
@@ -34,8 +34,8 @@ fifa2023(records=>{
       tableContents += `
             <tr>
                 <td data-label="Rank">${record.Ranks}</td>
-                <td data-label="Team">${record.Team}</td>
-                <td data-label="Total Points">${record.totalPoints}</td>
+                <td data-label="Team">${record.Teams}</td>
+                <td data-label="Total Points">${record.totalPoint}</td>
                 <td data-label="Rank">${record.Rank}</td>
                 <td data-label="Team">${record.Team}</td>
                 <td data-label="Total Points">${record.totalPoints}</td>
