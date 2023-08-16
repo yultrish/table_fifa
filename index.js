4dd5cc7ee3a3716185d7f75c2d7ab6ad3c05b637
yultrish <yultrish@gmail.com>
2023-08-16 08:50:12 +0000

    asyn-await using arrays

---
+++ index.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', ()=>{
+    tableContainer = document.querySelector('.table-container')
+
+ fifa2023 = (teams)=>{
+    setTimeout(()=>{
+        teams(
+           fifa = [
+        {Ranks:1, Team:'Brazil', totalPoints:1.841, Rank:16, Team:'Uruguay', totalPoints:1.627},
+        {Ranks:2, Team:'Argentina', totalPoints:1.83, Rank:17, Team:'Seraleon', totalPoints:1.5},
+        {Ranks:3, Team:'Morroco', totalPoints:1.821, Rank:18, Team:'Ghana', totalPoints:1.45},
+            ]
+        )
+    }, 5000)
+
+} 
+
+
+
+fifa2023(records=>{
+         tableContents = `
+      <table>
+        <thead>
+            <tr>
+                <th>Rank</th>
+                <th>Team</th>
+                <th>totalPoints</th>
+                <th>Rank</th>
+                 <th>Team</th>
+                  <th>totalPoints</th>
+            </tr>
+        </thead>
+        <tbody>`
+    records.forEach(record => {    
+      tableContents += `
+            <tr>
+                <td data-label="Rank">${record.Ranks}</td>
+                <td data-label="Team">${record.Team}</td>
+                <td data-label="Total Points">${record.totalPoints}</td>
+                <td data-label="Rank">${record.Rank}</td>
+                <td data-label="Team">${record.Team}</td>
+                <td data-label="Total Points">${record.totalPoints}</td>
+            </tr>          
+     ` 
+       
+    });
+       tableContents += `
+     </table>
+     </tbody>
+`
+    tableContainer.innerHTML = tableContents
+})
+})
+
